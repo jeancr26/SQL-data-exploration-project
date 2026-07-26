5f10986587e87a590cc282f90f86681e95cd950b
jeancr26 <jeancr26@gmail.com>
2026-07-25 22:08:22 -0400

    Create create_tables.sql

---
+++ database/create_tables.sql
@@ -0,0 +1,50 @@
+/*
+=====================================================
+Database: Store
+Purpose: Create product and manufacturer dataset
+Author: Jean Carlos
+=====================================================
+*/
+
+USE master;
+GO
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'Store')
+BEGIN
+    DROP DATABASE Store;
+END;
+GO
+
+CREATE DATABASE Store;
+GO
+
+USE Store;
+GO
+
+
+/*
+Create manufacturers table
+*/
+
+CREATE TABLE manufacturers
+(
+    manufacturer_id INT IDENTITY(1,1) PRIMARY KEY,
+    manufacturer_name VARCHAR(100) NOT NULL
+);
+
+
+/*
+Create products table
+*/
+
+CREATE TABLE products
+(
+    product_id INT IDENTITY(1,1) PRIMARY KEY,
+    product_name NVARCHAR(100) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    manufacturer_id INT NOT NULL,
+
+    CONSTRAINT FK_products_manufacturers
+    FOREIGN KEY (manufacturer_id)
+    REFERENCES manufacturers(manufacturer_id)
+);
